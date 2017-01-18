a4e9106163b59e4f13158fe3a45c2f41955a6604
Emmanuel Vo <emmanuelvo777@gmail.com>
2017-01-18 15:12:55 -0800

    Added the variables for calculating values

--- interest.js
+++ interest.js
@@ -1,1 +1,7 @@
-function calculate () {}
+function calculate () {
+  var amount = $('#amount').val();
+  var rate = $('#rate').val();
+  var month = $('#month').val();
+  var interestPercent = amount * (rate/100);
+  var total = Number(amount) * month * Number(rate);
+}
