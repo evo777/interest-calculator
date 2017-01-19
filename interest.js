d18363260cffeb6e267d1f076c0c5fa481b29d5c
Emmanuel Vo <emmanuelvo777@gmail.com>
2017-01-19 13:16:21 -0800

    Added notes in comment form

--- interest.js
+++ interest.js
@@ -1,10 +1,14 @@
 function calculate () {
+
+  //The values are from the input box in the html file.
+  //Whatever is input, the values will be stored in these variables.
   var amount = $('#amount').val();
   var rate = $('#rate').val();
   var month = $('#month').val();
   var interestPercent = amount * (rate/100);
   var total = Number(amount) * month * Number(rate);
 
+  //Rounding the value to two decimal places
   $('#total').val(total.toFixed(2));
 
   return false;
